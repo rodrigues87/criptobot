8b20459d77604e1ae4fb089ea2b5c9fdf350e09b
rodrigues87 <pedroh.mix@gmail.com>
2020-12-21 03:02:55 -0300

    Primeiro commit

---
+++ historico/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Historico(models.Model):
+    date = models.CharField(max_length=40)
+    open = models.DecimalField(max_digits=20, decimal_places=2)
+    high = models.DecimalField(max_digits=20, decimal_places=2)
+    aumenta = models.BooleanField()
+
+    def __str__(self):
+        return self.date
+
+
